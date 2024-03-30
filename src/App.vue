dd0820bcc2b70d1d99847d55cb8c6546a4477715
Alberto Salas <alsalasher@gmail.com>
2024-03-30 17:02:28 +0100

    update components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+
+        <RouterLink to="/counter">Counter</RouterLink>
       </nav>
     </div>
   </header>
